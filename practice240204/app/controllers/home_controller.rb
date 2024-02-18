6e789c27d4d96a5249be7fc1c76f17eca3547c84
MasashiFUJISHIRO <lammy725@gmail.com>
2024-02-18 16:14:04 +0900

    update: httpリクエストにクエリパラメタが付与されていればそれを読み取る処理のお試しを実装。

--- practice240204/app/controllers/home_controller.rb
+++ practice240204/app/controllers/home_controller.rb
@@ -1,15 +1,22 @@
 class HomeController < ApplicationController
 
   def index
-    msg = '
+    # URI にクエリパラメタが付与されていれば使用する。 (例) home/index?msg=Bob
+    if params['msg'] != nil then
+      msg = 'Hello ' + params['msg'] + '-san.'
+    else
+      msg = 'Hello world.   hoge piyo huga あいうえお'
+    end
+
+    my_html = '
       <html>
       <body>
         <h1>trial page 001</h1>
-        <p>Hello world.   hoge piyo huga あいうえお</p>
+        <p>' + msg + '</p>
       </body>
       </html>
       '
-      render html: msg.html_safe  # html_safe をつけることで html文がエスケープ処理されない
+      render html: my_html.html_safe  # html_safe をつけることで html文がエスケープ処理されない
   end
 
 end
