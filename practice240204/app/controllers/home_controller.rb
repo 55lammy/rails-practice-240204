c8730acf19d5da8ea0fc9f8f96e5d2467cb25e30
MasashiFUJISHIRO <lammy725@gmail.com>
2024-02-18 16:07:56 +0900

    update: プレーンテキストでなく html文を表示させるお試しコードを実装。

--- practice240204/app/controllers/home_controller.rb
+++ practice240204/app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-    render plain:"Hello World."
+    msg = '
+      <html>
+      <body>
+        <h1>trial page 001</h1>
+        <p>Hello world.   hoge piyo huga あいうえお</p>
+      </body>
+      </html>
+      '
+      render html: msg.html_safe  # html_safe をつけることで html文がエスケープ処理されない
   end
 
 end
