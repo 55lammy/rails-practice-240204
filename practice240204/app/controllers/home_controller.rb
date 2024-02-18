602020c6c1ca889c2f709f0c8e846be1a4423f05
MasashiFUJISHIRO <lammy725@gmail.com>
2024-02-18 17:42:14 +0900

    update: controllerに処理直書きでなく、viewsとscssのファイルを使って表示コンテンツを記述するよう変更。

--- practice240204/app/controllers/home_controller.rb
+++ practice240204/app/controllers/home_controller.rb
@@ -1,22 +1,24 @@
 class HomeController < ApplicationController
 
   def index
-    # URI にクエリパラメタが付与されていれば使用する。 (例) home/index?msg=Bob
-    if params['msg'] != nil then
-      msg = 'Hello ' + params['msg'] + '-san.'
-    else
-      msg = 'Hello world.   hoge piyo huga あいうえお'
-    end
+    # views/home/index.html.erb の内容を表示させる
 
-    my_html = '
-      <html>
-      <body>
-        <h1>trial page 001</h1>
-        <p>' + msg + '</p>
-      </body>
-      </html>
-      '
-      render html: my_html.html_safe  # html_safe をつけることで html文がエスケープ処理されない
+    # # URI にクエリパラメタが付与されていれば使用する。 (例) home/index?msg=Bob
+    # if params['msg'] != nil then
+    #   msg = 'Hello ' + params['msg'] + '-san.'
+    # else
+    #   msg = 'Hello world.   hoge piyo huga あいうえお'
+    # end
+
+    # my_html = '
+    #   <html>
+    #   <body>
+    #     <h1>trial page 001</h1>
+    #     <p>' + msg + '</p>
+    #   </body>
+    #   </html>
+    #   '
+    #   render html: my_html.html_safe  # html_safe をつけることで html文がエスケープ処理されない
   end
 
 end
