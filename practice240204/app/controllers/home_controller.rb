15438f74da3df1afafb0d05d41f359fa937afb9b
MasashiFUJISHIRO <lammy725@gmail.com>
2024-03-03 16:20:39 +0900

    update: viewsにて、controllerから渡した変数を表示するお試しコードの実装。パーシャルファイルをrenderで呼び出すお試しコードの実装。

--- practice240204/app/controllers/home_controller.rb
+++ practice240204/app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
 
+  # [240303]
+  # app/views/layouts/study01.html.erb を使用する指定。指定なしの場合、 index.html.erb をデフォルトで使用する。
+  layout 'study01'
+
+  
   def index
+    # [240303]
+    @header = 'layout header study01 240303'
+    @footer = 'copyright M-YAMA 2024'
+    @title = 'tab title 240303'
+    @msg = 'study01 240303 test hoge piyo'
+    
+    
+    # [240218]
     # views/home/index.html.erb の内容を表示させる
 
     # # URI にクエリパラメタが付与されていれば使用する。 (例) home/index?msg=Bob
@@ -18,7 +31,7 @@ class HomeController < ApplicationController
     #   </body>
     #   </html>
     #   '
-    #   render html: my_html.html_safe  # html_safe をつけることで html文がエスケープ処理されない
+    # render html: my_html.html_safe  # html_safe をつけることで html文がエスケープ処理されない
   end
 
 end
